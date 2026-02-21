fec1afad9a3f6e069a1da8a4686d1c0c431cd541
BLACKVENOM499 <blackvenomx499@gmail.com>
2026-02-21 13:25:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "OUpHSRbB#eb8Bbmlv8azgTmnxVoL3YZai7zOA_1XTUcQuJYYZbpo",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "6ZxWCA7R#0vRpYEMogifJCrJ0Rtwn99jAsMbq67dBds5fBTrXckI",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "ᴀᴋɪɴᴅᴜ-ᴍᴅ",  // Bot's display name
